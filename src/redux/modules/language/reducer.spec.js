a26d03676d9607ad467295e0632daacec30c9386
Markus Dethlefsen <privat@markus-inger.de>
2016-05-27 17:00:38 +0200

    some code style

--- src/redux/modules/language/reducer.spec.js
+++ src/redux/modules/language/reducer.spec.js
@@ -1,27 +1,24 @@
-import { expect } from 'chai';
+import { expect } from 'chai'
 import deepFreeze from 'deep-freeze'
 
 import languageReducer from './index'
 import { setLanguage } from './actions'
 
 describe('reducer language', () => {
+  it('set the current language', () => {
+    const stateBefore = {
+      current: 'en',
+      available: ['en', 'de']
+    }
+    const action = setLanguage('de')
+    const stateAfter = {
+      current: 'de',
+      available: ['en', 'de']
+    }
 
-    it('set the current language', () => {
+    deepFreeze(stateBefore)
+    deepFreeze(action)
 
-        const stateBefore = {
-            current: 'en',
-            available: ['en', 'de']
-        }
-        const action = setLanguage('de')
-        const stateAfter = {
-            current: 'de',
-            available: ['en', 'de']
-        }
-
-        deepFreeze(stateBefore)
-        deepFreeze(action)
-
-        expect(languageReducer(stateBefore, action)).to.deep.equal(stateAfter)
-    });
-
-});
+    expect(languageReducer(stateBefore, action)).to.deep.equal(stateAfter)
+  })
+})
