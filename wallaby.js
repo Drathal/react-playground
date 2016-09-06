75ae0e51dc582cb623b56bcc360a1354c69d2a68
Markus Dethlefsen <privat@markus-dethlefsen.de>
2016-09-06 13:13:32 +0200

    fix wallaby config

--- wallaby.js
+++ wallaby.js
@@ -1,9 +1,9 @@
-/* eslint global-require: "off" */
 process.env.BABEL_DISABLE_CACHE = 1
 process.env.BABEL_ENV = process.env.NODE_ENV = 'test'
 const Fs = require('fs')
 const Path = require('path')
-const babelConfiguration = JSON.parse(Fs.readFileSync(Path.join(__dirname, '.babelrc')))
+
+const babelConfiguration = JSON.parse(Fs.readFileSync(Path.join(__dirname, '.babelrc'))).env.test
 babelConfiguration.babel = require('babel-core')
 
 module.exports = (wallaby) => {
@@ -11,20 +11,24 @@ module.exports = (wallaby) => {
     files: [
       { pattern: 'test/helper.js' },
       { pattern: 'src/**/*.js' },
-      { pattern: '!src/**/*.spec.js' }
+      { pattern: 'src/**/*.jsx' },
+      { pattern: '!src/**/*.spec.js' },
+      { pattern: '!src/**/*.spec.jsx' }
     ],
     tests: [
       { pattern: 'src/**/*.spec.js' },
+      { pattern: 'src/**/*.spec.jsx' }
     ],
     compilers: {
-      'src/**/*.js': wallaby.compilers.babel(babelConfiguration)
+      'src/**/*.js': wallaby.compilers.babel(babelConfiguration),
+      'src/**/*.jsx': wallaby.compilers.babel(babelConfiguration)
     },
     env: {
       type: 'node'
     },
     testFramework: 'mocha',
     bootstrap() {
-      require('./test/helper.js')
+      require('./test/helper')
     },
     debug: false
   }
